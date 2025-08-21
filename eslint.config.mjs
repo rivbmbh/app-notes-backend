dcbe031815c862be767f7cebe81a7dd4b017d83c
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-08-22 07:54:27 +0800

    feat: buiild http server

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import daStyle from "eslint-config-dicodingacademy";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import daStyle from 'eslint-config-dicodingacademy';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
   },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
   daStyle,
 ]);
--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.info("hellow");
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+// eslint-disable-next-line linebreak-style
+init();
