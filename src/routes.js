4ae1e3dca444b677eaf8c07a21039899a18b767b
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-08-22 20:39:36 +0800

    feat: get note by id

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
-const { addNoteHandler } = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNotesByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
-    // options: {
-    //   cors: {
-    //     origin: ["*"],
-    //   },
-    // },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotesByIdHandler,
   },
 ];
 
